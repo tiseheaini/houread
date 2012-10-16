954f6df176fe718444f24b63a537baeb2c40a392
tiseheaini <tiseheaini@gmail.com>
2012-10-16 11:08:42 +0800

    新建了一个项目 houread

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Houread::Application.config.secret_token = 'b8104c851625507b8ce37cc025c9f6e2913f549d9a3b9b750968ea700f01263a543584178ec724b693634f9bd42cde4456fc0e0e5608e3cec66eaa066171bc46'
