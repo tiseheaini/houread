5173feca5179e02b9bc73c49a06c8807fc256a5f
tiseheaini <tiseheaini@gmail.com>
2012-10-24 21:17:42 +0800

    出现 BUG ，分钟大于 1 分钟时无法减时间，导致始终无法刷新页面。 修复了出现的 BUG ，并修改了 rails 时区的设置

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,26 +5,27 @@ var countdown = function(){
   var time = $("#time").html();
   var h    = 0;
   var m    = parseInt(time / 60);
-  var s    = 60;
+  var s    = time % 60;
 
   // 设置倒计时方法
 	var settime = function(){
-	  if (s == 60){
-		  m -= 1;
-		}
-		if (s == 0){
-		  if (m == 0){
-			  $("#countdown").css("visibility","hidden")
-				window.location.href="/home/index";
-			}else{
-			  s = 60;
-			}
-		}
-  
-    s -= 1;
-  
-	  $("#countdown").html('<p><span>还有'+h+' 小时'+m+' 分钟'+s+'秒</span></p>')
-  }
+	  if (h == 0 && m == 0 && s == 0) {
+	    $("#countdown").css("visibility","hidden");
+	    window.location.href="/home/index";
+	  }
+	  if (s == 0) { s = 60; }
+	  if (s == 60) {
+	    m -= 1;
+
+	    if (m == 0 && h > 0) {
+	      h -= 1;
+	      m = 60;
+	      s = 60;
+	    }
+	  }
+	  s -= 1;
+		$("#countdown").html('<p><span>还有'+h+' 小时'+m+' 分钟'+s+'秒</span></p>')
+	}
 
   // 开始执行程序
   setInterval(settime, 1000);
