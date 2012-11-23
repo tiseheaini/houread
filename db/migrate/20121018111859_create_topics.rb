9ac806c7ebf4d6a15a1d0b14c83a48bfa6b60e54
tiseheaini <tiseheaini@gmail.com>
2012-11-23 15:05:14 +0800

    修改数据库
      修改了几次数据库结构，likeable 和 unlikeable 使用 text

--- db/migrate/20121018111859_create_topics.rb
+++ db/migrate/20121018111859_create_topics.rb
@@ -2,8 +2,8 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.text :body,     :null => false
-      t.string :likeable,   :default => ""
-      t.string :unlikeable, :default => ""
+      t.text :likeable
+      t.text :unlikeable
       t.integer :timeint
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -23,12 +23,12 @@ ActiveRecord::Schema.define(:version => 20121021150920) do
   end
 
   create_table "topics", :force => true do |t|
-    t.text     "body",                       :null => false
-    t.string   "likeable",   :default => ""
-    t.string   "unlikeable", :default => ""
+    t.text     "body",       :null => false
+    t.text     "likeable"
+    t.text     "unlikeable"
     t.integer  "timeint"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
