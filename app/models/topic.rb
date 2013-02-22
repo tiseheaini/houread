1c8c90806f0803fb9dcced453c7360e2e045b3cd
tiseheaini <tiseheaini@gmail.com>
2013-02-22 16:26:25 +0800

    把topic.rb文件中的方法添加上最简单的注释，将来还会有改动

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
   attr_accessible :title, :body, :like, :unlike, :timeint
 
+  ## add_timeint 用于将传进来的timeint加1，且考虑时间问题
   def self.add_timeint(timeint)
   	if timeint.to_s[8..9].to_i + 1 > 19
       time =  Time.local(timeint.to_s[0..3], timeint.to_s[4..5], timeint.to_s[6..7]).tomorrow
@@ -10,6 +11,7 @@ class Topic < ActiveRecord::Base
     end
   end
 
+  ## make_timeint 用于生成当前时间的timeint串
   def self.make_timeint
     time = Time.now
     if (time.hour - 6) > 12
@@ -20,6 +22,7 @@ class Topic < ActiveRecord::Base
     end
   end
   
+  ## del_timeint 用于将传进来的timeint减1，且考虑时间问题
   def self.del_timeint(timeint)
     if timeint.to_s[8..9].to_i - 7 <= 0
       time =  Time.local(timeint.to_s[0..3], timeint.to_s[4..5], timeint.to_s[6..7]).yesterday
