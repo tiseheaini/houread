95a264116a49b42b7f6070f14cd5ef3e508c3433
tiseheaini <tiseheaini@gmail.com>
2013-03-13 22:20:32 +0800

    更改主页的显示方式
    在 home#index里面判断 @topic是否有值，然后去渲染 content还是 no_content
    根据时间来决定是否渲染 close_content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @topic = Topic.last
-    @topiclike = @topic.likeable.to_s.split.length
     @time = 120 - Time.now.min
+
+    current_hour = Time.now.hour
+    if (9..21).include?(current_hour)
+      time = Time.now
+      timeint = time.year.to_s + ("%02d" % time.month) + ("%02d" % time.day) + ("%02d" % ((time.hour - 9)/2))
+      @topic = Topic.where(:timeint => timeint)
+      @topiclike = @topic.likeable.to_s.split.length if @topic.present?
+    else
+      render :template => 'home/close_content'
+    end
   end
 end
