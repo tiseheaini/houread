755a2a1634b1aa17ff8e96a719292ecc8333d8ce
tiseheaini <tiseheaini@gmail.com>
2013-03-17 23:42:33 +0800

    将每两小时一篇文章改回每小时一篇 -b
    修改了 Topic#add_timeint 方法
    测试 2013031700.upto(2013031711){|i|atime=Topic.add_timeint(i.to_s);puts i.to_s+'-->'+atime}

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,7 @@ class Topic < ActiveRecord::Base
 
   ## add_timeint 用于将传进来的timeint加1，且考虑时间问题
   def self.add_timeint(timeint)
-    if timeint[8..9].to_i >= 5
+    if timeint[8..9].to_i >= 11
       time =  Time.local(timeint[0..3], timeint[4..5], timeint[6..7]).tomorrow
       timeint = time.year.to_s + ("%02d" % time.month) + ("%02d" % time.day) + "00"
     else
