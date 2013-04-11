f1bc0757d2a3b4ceb26a4283823a9bed8f5da8b0
tiseheaini <tiseheaini@gmail.com>
2013-04-12 01:04:08 +0800

    修改了 home_controller中 @topic查出的 bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     if (9..21).include?(current_hour)
       time = Time.now
       timeint = time.year.to_s + ("%02d" % time.month) + ("%02d" % time.day) + ("%02d" % ((time.hour - 9)/2))
-      @topic = Topic.where(:timeint => timeint)
+      @topic = Topic.where(:timeint => timeint).first
       @topiclike = @topic.likeable.to_s.split.length if @topic.present?
     else
       render :template => 'home/close_content'
