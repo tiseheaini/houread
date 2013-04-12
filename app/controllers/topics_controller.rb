3ae492bb1e0cb3b4990cab79869a166b25d6d443
tiseheaini <tiseheaini@gmail.com>
2013-04-12 19:35:30 +0800

    /yuedu 页面分享按钮不能使用，修复

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -24,11 +24,15 @@ class TopicsController < ApplicationController
   end
 
   def encoding
+    @time = 3600 - (Time.now.min * 60 + Time.now.sec)
     coding = Topic.decoding(params[:encoding])
+    @share_site = request.url
 
     @topic = Topic.where(:timeint => coding).first
-    @topiclike = @topic.likeable.to_s.split.length if @topic.present?
-    @time = 3600 - (Time.now.min * 60 + Time.now.sec)
+    if @topic.present?
+      @topiclike = @topic.likeable.to_s.split.length
+      @title = Sanitize.clean(@topic.title).strip
+    end
 
     render :template => 'home/index'
   end
