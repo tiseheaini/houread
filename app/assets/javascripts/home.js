1c80e3f3297b399421c27f05fbcf8bc0d478c0b6
tiseheaini <tiseheaini@gmail.com>
2013-04-15 00:29:45 +0800

    删除页面倒计时功能

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,34 +1,4 @@
 
-var countdown = function(){
-  var time = $("#time").html();
-  var h    = 0;
-  var m    = parseInt(time / 60);
-  var s    = time % 60;
-
-  // 设置倒计时方法
-	var settime = function(){
-	  if (h == 0 && m == 0 && s == 0) {
-	    $("#countdown").css("visibility","hidden");
-	    window.location.href="/";
-	  }
-	  if (s == 0) { s = 60; }
-	  if (s == 60) {
-	    m -= 1;
-
-	    if (m == 0 && h > 0) {
-	      h -= 1;
-	      m = 60;
-	      s = 60;
-	    }
-	  }
-	  s -= 1;
-		$("#countdown").html('<p><span>离下一篇文章出现还有'+m+' 分钟'+s+'秒</span></p>')
-	}
-
-  // 开始执行程序
-  setInterval(settime, 1000);
-}
-
 var likeable = function(){
   var fadetime = 900;
   $(".btn-like").click(
