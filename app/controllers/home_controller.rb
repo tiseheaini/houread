97e08ff2ac224214380479f8ef6db01f342f24d2
tiseheaini <tiseheaini@gmail.com>
2013-04-15 19:36:28 +0800

    重写了 home#index页面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @time = 3600 - (Time.now.min * 60 + Time.now.sec)
+    ## 获取分享按钮的地址
+    current_timeint = Topic.current_timeint(Time.now)
+    share_site_topic = Topic.where(:timeint => current_timeint).first
+    if share_site_topic.present?
+      encoding_site = Topic.encoding(share_site_topic.timeint)
+      @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
+    end
 
-    current_hour = Time.now.hour
-    if (9..20).include?(current_hour)
-      ## 获取分享按钮的地址
-      current_timeint = Topic.current_timeint(Time.now)
-      share_site_topic = Topic.where(:timeint => current_timeint).first
-      if share_site_topic.present?
-        encoding_site = Topic.encoding(share_site_topic.timeint)
-        @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
-      end
-      #time = Time.now
-      #timeint = time.year.to_s + ("%02d" % time.month) + ("%02d" % time.day) + ("%02d" % ((time.hour - 9)/2))
-      timeint = Topic.current_timeint(Time.now)
-      @topic = Topic.where(:timeint => timeint).first
-      if @topic.present?
-        @topiclike = @topic.likeable.to_s.split.length
-        @title = Sanitize.clean(@topic.title).strip
-      end
-    else
-      render :template => 'home/close_content'
+    @topic = Topic.where(:timeint => current_timeint).first
+    if @topic.present?
+      @topiclike = @topic.likeable.to_s.split.length
+      @title = Sanitize.clean(@topic.title).strip
     end
   end
 end
