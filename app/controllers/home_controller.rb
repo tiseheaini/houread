58fe56f8b3035430ee0d996f822c5545f149a89a
tiseheaini <tiseheaini@gmail.com>
2013-04-19 17:09:58 +0800

    修改 home_controller 使其在没有文章时读取最后一篇文章

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,13 @@ class HomeController < ApplicationController
   def index
     ## 获取分享按钮的地址
     current_timeint = Topic.current_timeint(Time.now)
-    share_site_topic = Topic.where(:timeint => current_timeint).first
+    share_site_topic = Topic.where(:timeint => current_timeint).first || Topic.last
     if share_site_topic.present?
       encoding_site = Topic.encoding(share_site_topic.timeint)
       @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
     end
 
-    @topic = Topic.where(:timeint => current_timeint).first
+    @topic = Topic.where(:timeint => current_timeint).first || Topic.last
     if @topic.present?
       @topiclike = @topic.likeable.to_s.split.length
       @title = Sanitize.clean(@topic.title).strip
