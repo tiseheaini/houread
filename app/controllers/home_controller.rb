04dbb71998546ae465241784176a6b5ba47e848f
tiseheaini <tiseheaini@gmail.com>
2013-04-19 17:31:15 +0800

    将 home_controller 中 before_filter 去掉

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,28 +1,23 @@
 class HomeController < ApplicationController
   include Home
-  before_filter :mobile
 
   def index
-    ## 获取分享按钮的地址
-    current_timeint = Topic.current_timeint(Time.now)
-    share_site_topic = Topic.where(:timeint => current_timeint).first || Topic.last
-    if share_site_topic.present?
-      encoding_site = Topic.encoding(share_site_topic.timeint)
-      @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
-    end
-
-    @topic = Topic.where(:timeint => current_timeint).first || Topic.last
-    if @topic.present?
-      @topiclike = @topic.likeable.to_s.split.length
-      @title = Sanitize.clean(@topic.title).strip
-    end
-  end
-
-  private
-
-  def mobile
     if mobile?
       redirect_to mobiles_path
+    else
+      ## 获取分享按钮的地址
+      current_timeint = Topic.current_timeint(Time.now)
+      share_site_topic = Topic.where(:timeint => current_timeint).first || Topic.last
+      if share_site_topic.present?
+        encoding_site = Topic.encoding(share_site_topic.timeint)
+        @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
+      end
+
+      @topic = Topic.where(:timeint => current_timeint).first || Topic.last
+      if @topic.present?
+        @topiclike = @topic.likeable.to_s.split.length
+        @title = Sanitize.clean(@topic.title).strip
+      end
     end
   end
 end
