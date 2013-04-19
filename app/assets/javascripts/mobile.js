904d58fc6f93927010e987c4740645b4e1db15b1
tiseheaini <tiseheaini@gmail.com>
2013-04-19 15:18:46 +0800

    mobile 基本功能完成

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,4 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //
+//= require jquery
 //= require_self
+
+var likeable = function(){
+  var fadetime = 900;
+  $(".btn-like").click(
+    function(){
+      var topicId = $(".btn-like").data("topic");
+      $.get("/topics/likeable" + "?" + "topic_id=" + topicId, 
+          function(data){
+            if (data.status){
+              $(".like-info").css("display","block");
+              $(".like-info").html(data.text);
+              $(".btn-like span").html("喜欢 " + data.count);
+              $(".like-info").fadeOut(fadetime);
+            }else{
+              $(".like-error-info").css("display","block");
+              $(".like-error-info").html(data.text);
+              $(".like-error-info").fadeOut(fadetime);
+            }
+          }, "json"
+      )
+    }
+  )
+}
--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,6 +1,18 @@
 class MobileController < ApplicationController
 
   def index
-    
+    ## 获取分享按钮的地址
+    current_timeint = Topic.current_timeint(Time.now)
+    share_site_topic = Topic.where(:timeint => current_timeint).first
+    if share_site_topic.present?
+      encoding_site = Topic.encoding(share_site_topic.timeint)
+      @share_site = "http://www.shiyueqingxin.com/yuedu/" + encoding_site
+    end
+
+    @topic = Topic.where(:timeint => current_timeint).first
+    if @topic.present?
+      @topiclike = @topic.likeable.to_s.split.length
+      @title = Sanitize.clean(@topic.title).strip
+    end
   end
 end
